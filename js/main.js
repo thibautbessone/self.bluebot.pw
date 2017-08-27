2581b53424f8087a43ddff8819c776ebc5bdaac8
thibautbessone <thibaut13b@gmail.com>
2017-08-27 18:35:56 +0200

    First push

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        connectParticles: true,
+        color: '#f5f5f5',
+        maxParticles: 150,
+        speed: 1
+    });
+};
